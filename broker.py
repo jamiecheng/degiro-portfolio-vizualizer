9ee83130c929ee337cc468276e0de61b29f7a24d
Jamie <jamiecheng97@gmail.com>
2021-08-23 23:06:29 +0200

    addded function to retrieve historical stock data with degiro products

--- broker.py
+++ broker.py
@@ -1,5 +1,5 @@
 import degiroapi
-from datetime import datetime
+from datetime import datetime, timedelta
 import local_config
 import pandas as pd
 
@@ -21,6 +21,7 @@ class Broker:
 
             tsn = {
                 'date': date.strftime('%Y-%m-%d'),
+                'id': transaction['productId'],
                 'name': info['name'],
                 'ticker': info['symbol'],
                 'quantity': transaction['quantity'],
@@ -34,10 +35,43 @@ class Broker:
 
         return df.set_index('date')
 
+    def get_product_history(self, product_ids, start_date, end_date):
+        dfs = []
+
+        for product_id in product_ids:
+            res = self.degiro.real_time_price(product_id, degiroapi.Interval.Type.Max)
+
+            # reformat the retarded data format
+            start = datetime.strptime(res[1]['times'][0:len(res[1]['times']) - 4], '%Y-%m-%d')
+
+            stock_data = []
+            for price in res[1]['data']:
+                data = {
+                    'date': start + timedelta(days=price[0]),
+                    res[0]['data']['alfa']: price[1]
+                }
+
+                stock_data.append(data)
+
+            df = pd.DataFrame(stock_data).set_index('date')
+
+            dfs.append(df)
+
+        df = pd.concat(dfs, axis=1, join='outer')
+
+        return df.loc[start_date.strftime('%Y-%m-%d'): end_date.strftime('%Y-%m-%d')]
+
     def get_cash_funds(self):
-        cashfunds = self.degiro.getdata(degiroapi.Data.Type.CASHFUNDS)
-        print(cashfunds)
+        return self.degiro.getdata(degiroapi.Data.Type.CASHFUNDS)
 
     def logout(self):
         self.degiro.logout()
 
+
+account = Broker()
+
+print(account.login(local_config.username, local_config.password))
+
+print(account.get_product_history([1157277, 1147582], datetime(2010, 1, 1), datetime.now()))
+
+account.logout()
