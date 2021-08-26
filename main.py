3587c796227e3eacfef8214b5ea6a0d49ceea49f
jchengDH <63774725+jchengDH@users.noreply.github.com>
2021-08-26 15:04:50 +0200

    main example plots the correlation matrix

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ if __name__ == '__main__':
     print('Sharpe ratio : {:.2f}'.format(p.get_sharpe()))
     print('Portfolio gain : {:.2f}%'.format(p.get_profit_loss()))
     print(p.get_allocation())
-    print(p.get_stocks_correlation(datetime(2010, 1, 1), datetime.now()))
 
     p.get_value_over_time().plot()
+
+    plt.matshow(p.get_stocks_correlation(datetime(2010, 1, 1), datetime.now()))
 
     plt.show()
